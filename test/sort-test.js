8f9667c98198a8a36d4a566dfeadf1f4194a906a
Brad Niedt <bradleyniedt@gmail.com>
2019-03-07 14:48:13 -0700

    Adds tests for the check helper function.

--- test/sort-test.js
+++ test/sort-test.js
@@ -9,4 +9,16 @@ describe('Sorting functionality', function() {
       assert.deepEqual(sorter.sortMe(toSort), sorted)
     });
   });
+  context('check', function() {
+    it('can determine if an array is sorted', function() {
+      var toSort = [5,2,4,3,1];
+      var sorted = [1,2,3,4,5];
+      assert.equal(sorter.check(sorted), true)
+    });
+    it('can determine if an array is not sorted', function() {
+      var toSort = [5,2,4,3,1];
+      var sorted = [1,2,3,4,5];
+      assert.equal(sorter.check(toSort), false)
+    });
+  });
 });
